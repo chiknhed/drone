9a80082c3e281adcee21901e2f9535c2ff461a6b
Owen Kwon <ohkeunow@qti.qualcomm.com>
2015-11-21 04:20:43 +0900

    higher version trial from nav6 code

--- drone.ino
+++ drone.ino
@@ -5,18 +5,31 @@
  *  
  */
 
-#include <I2Cdev.h>
+#define EMPL_TARGET_ATMEGA328
+
 #include <Wire.h>
+#include <I2Cdev.h>
 #include <PID_v1.h>
+#include <math.h>
 #include <helper_3dmath.h>
-#include <MPU6050_6Axis_MotionApps20.h>
 #include <PinChangeInt.h>
+extern "C" {
+  #include <inv_mpu.h>
+  #include <inv_mpu_dmp_motion_driver.h>
+}
 
 //#define VERBOSE_DEBUG
 
 #define PID_GYRO_P            4.7
 #define PID_GYRO_I            0.9
 #define PID_GYRO_D            2.5
+
+#define SDA_PIN     (20)
+#define SCL_PIN     (21)
+
+#define DEFAULT_MPU_HZ     (200)
+#define MAX_NAV6_MPU_RATE   (500)
+#define MIN_NAV6_MPU_RATE   (4)
 
 #define RC_1  A9
 #define RC_2  A10
@@ -26,11 +39,7 @@
 #define RC_6  A14
 #define RC_7  A15
 
-#define PID_SAMPLE_PERIOD     5
-
-#define PRESAMPLE_COUNT     500
-#define PRESAMPLE_STABLE_CHECK  30
-#define PRESAMPLE_STABLE_TOLERANCE  100.0
+#define PID_SAMPLE_PERIOD     1
 
 #define ESC_MIN               88
 #define ESC_WORKING_MIN       160
@@ -79,30 +88,51 @@
 
 #define PID_XY_INFLUENCE    30.0
 
-double adj_accel_z = 0, adj_gyro_x = 0, adj_gyro_y = 0;
+double adj_gyro_x = 0, adj_gyro_y = 0;
 double accel_z, gyro_x, gyro_y;
 
 double v_ac, v_bd, velocity, bal_axes;
 
-int presample_count  = PRESAMPLE_COUNT;
-boolean resample_sensor = false;
+unsigned char dmp_on;
+volatile bool new_gyro;
 
-MPU6050 mpu;
-Quaternion q;                          // quaternion for mpu output
-VectorFloat gravity;
-int16_t accel_data[3];
-float ypr[3] = {0.0f,0.0f,0.0f};       // yaw pitch roll values
-float yprLast[3] = {0.0f, 0.0f, 0.0f};
+
+/* Conversion Factors */
+const float degrees_to_radians = M_PI / 180.0;
+const float radians_to_degrees = 180.0 / M_PI;
+
+/* MPU Calibration */
+#define STARTUP_CALIBRATION_DELAY_MS        19000
+#define CALIBRATED_OFFSET_AVERAGE_PERIOD_MS  1000
+
+#define NAV6_CALIBRATION_STATE_WAIT 0 // Waiting for MPU to complete internal calibration
+#define NAV6_CALIBRATION_STATE_ACCUMULATE  1 // Accumulate Yaw/Pitch/Roll offsets
+#define NAV6_CALIBRATION_STATE_COMPLETE    2 // Normal Operation
+
+int calibration_state = NAV6_CALIBRATION_STATE_WAIT;
+int calibration_accumulator_count = 0;
+float yaw_accumulator = 0.0;
+float quaternion_accumulator[4] = { 0.0, 0.0, 0.0, 0.0 };
+float calibrated_yaw_offset = 0.0;
+float calibrated_quaternion_offset[4] = { 0.0, 0.0, 0.0, 0.0 };
+
+/* Gyro / Accel / DMP State */
+float ypr[3] = {0, 0, 0};
+unsigned long sensor_timestamp;
+
+struct FloatVectorStruct {
+  float x;
+  float y;
+  float z;
+};
+struct FloatVectorStruct gravity;
+
+unsigned char accel_fsr;  // accelerometer full-scale rate, in +/- Gs (possible values are 2, 4, 8 or 16).  Default:  2
+unsigned short dmp_update_rate; // update rate, in hZ (possible values are between 4 and 1000).  Default:  100
+unsigned short gyro_fsr;  // Gyro full-scale_rate, in +/- degrees/sec, possible values are 250, 500, 1000 or 2000
+
 
 volatile bool interruptLock = false;
-
-uint8_t mpuIntStatus;                  // mpu statusbyte
-uint8_t in_error;                     // device status    
-uint16_t packetSize;                   // estimated packet size  
-uint16_t fifoCount;                    // fifo buffer size   
-uint8_t fifoBuffer[64];                // fifo buffer 
-
-volatile bool mpuInterrupt = false;    //interrupt flag
 
 double ch1, ch2, ch3, ch4;
 int ch5, ch6, ch7;
@@ -129,47 +159,183 @@ PID xPID(&gyro_x, &v_ac, &adj_gyro_x, pid_gyro_p, pid_gyro_i, pid_gyro_d, DIRECT
 PID yPID(&gyro_y, &v_bd,  &adj_gyro_y, pid_gyro_p, pid_gyro_i, pid_gyro_d, DIRECT);
 
 void setup() {
-  initRC();
-  
-  Serial.begin(115200);
+  resetI2C();
 
   Wire.begin();
-  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+  Wire.setClock(400000L);
+  
+  Serial.begin(115200);
+  Serial.flush();
 
-  pinMode(13, OUTPUT);
-
-  Serial.println(F("Init Servos.."));
-  initServo();
-
-  Serial.println(F("Arming.."));
-  arm(1);
+  Serial.println(F("MPU init.."));
+  Serial.flush();
+  initMPU();
 
   Serial.println(F("Init PID.."));
+  Serial.flush();
   xPID.SetMode(AUTOMATIC);
   xPID.SetSampleTime(PID_SAMPLE_PERIOD);
   xPID.SetOutputLimits(-PID_XY_INFLUENCE, PID_XY_INFLUENCE);
   yPID.SetMode(AUTOMATIC);
   yPID.SetSampleTime(PID_SAMPLE_PERIOD);
   yPID.SetOutputLimits(-PID_XY_INFLUENCE, PID_XY_INFLUENCE);
-    
-  Serial.println(F("MPU init.."));
-  initMPU();
+  
+  Serial.println(F("RC Interrupts Init.."));
+  initRC();
+
+  Serial.println(F("Init Servos.."));
+  initServo();
+
+  Serial.println(F("Arming.."));
+  arm(1);
 }
 
 void loop() {
-  while(!mpuInterrupt && fifoCount < packetSize){
+  if (new_gyro && dmp_on) {
+    short gyro[3], accel[3], sensors;
+    unsigned char more = 0;
+    long quat[4];
+    //float euler[3];
+    /* This function gets new data from the FIFO when the DMP is in
+     * use. The FIFO can contain any combination of gyro, accel,
+     * quaternion, and gesture data. The sensors parameter tells the
+     * caller which data fields were actually populated with new data.
+     * For example, if sensors == (INV_XYZ_GYRO | INV_WXYZ_QUAT), then
+     * the FIFO isn't being filled with accel data.
+     * The driver parses the gesture data to determine if a gesture
+     * event has occurred; on an event, the application will be notified
+     * via a callback (assuming that a callback function was properly
+     * registered). The more parameter is non-zero if there are
+     * leftover packets in the FIFO.
+     */
+    int success = dmp_read_fifo(gyro, accel, quat, &sensor_timestamp, &sensors,
+                                &more);
     
+    if (!more)
+      new_gyro = 0;
+
+    if ( ( success == 0 ) && ( (sensors & INV_XYZ_ACCEL) != 0 ) && ( (sensors & INV_WXYZ_QUAT) != 0 ) ) {
+
+      Quaternion q( (float)(quat[0] >> 16) / 16384.0f,
+                    (float)(quat[1] >> 16) / 16384.0f,
+                    (float)(quat[2] >> 16) / 16384.0f,
+                    (float)(quat[3] >> 16) / 16384.0f);
+
+      // Calculate Yaw/Pitch/Roll
+      // Update client with yaw/pitch/roll and tilt-compensated magnetometer data
+
+      getGravity(&gravity, &q);
+      dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+      boolean accumulate = false;
+      if ( calibration_state == NAV6_CALIBRATION_STATE_WAIT ) {
+
+        digitalWrite(13, HIGH);
+
+        if ( millis() >= STARTUP_CALIBRATION_DELAY_MS ) {
+
+          calibration_state = NAV6_CALIBRATION_STATE_ACCUMULATE;
+        }
+      }
+      if ( calibration_state == NAV6_CALIBRATION_STATE_ACCUMULATE ) {
+
+        accumulate = true;
+        if ( millis() >= (STARTUP_CALIBRATION_DELAY_MS + CALIBRATED_OFFSET_AVERAGE_PERIOD_MS) ) {
+
+          accumulate = false;
+          calibrated_yaw_offset = yaw_accumulator / calibration_accumulator_count;
+          calibrated_quaternion_offset[0] = quaternion_accumulator[0] / calibration_accumulator_count;
+          calibrated_quaternion_offset[1] = quaternion_accumulator[1] / calibration_accumulator_count;
+          calibrated_quaternion_offset[2] = quaternion_accumulator[2] / calibration_accumulator_count;
+          calibrated_quaternion_offset[3] = quaternion_accumulator[3] / calibration_accumulator_count;
+          calibration_state = NAV6_CALIBRATION_STATE_COMPLETE;
+          digitalWrite(13, LOW);
+        }
+        else {
+
+          calibration_accumulator_count++;
+
+        }
+      }
+
+      float x = ypr[0] * radians_to_degrees;
+      float y = ypr[1] * radians_to_degrees;
+      float z = ypr[2] * radians_to_degrees;
+
+      if ( accumulate ) {
+
+        yaw_accumulator += x;
+        quaternion_accumulator[0] += q.w;
+        quaternion_accumulator[1] += q.x;
+        quaternion_accumulator[2] += q.y;
+        quaternion_accumulator[3] += q.z;
+
+      }
+
+      gyro_x = z;
+      gyro_y = - y;
+      accel_z = 0;
+
+      if (calibration_state == NAV6_CALIBRATION_STATE_COMPLETE) position_adjust();
+    }
+    else {
+
+      /* The following debug print outs are useful
+         if DMP fifo streaming is not working or
+         data is being lost.
+         If modifying code, it's a good idea to check that
+         the "FIFO OVERFLOW" error does not occur, since
+         this case can occur if too many cycles are used
+         to keep up with the sensor data stream. */
+      if ( success == -1 )
+      {
+        Serial.println(F("DMP DISABLED!!!"));
+      }
+      else if ( success == -2 )
+      {
+        Serial.println(F("I2C READ ERROR"));
+      }
+      else if ( success == -3 )
+      {
+        Serial.println(F("FIFO OVERFLOW ERROR!!!"));
+      }
+      else if ( success == -4 )
+      {
+        Serial.println(F("NO_SENSORS"));
+      }
+      else if ( success == -6 )
+      {
+        Serial.println(F("CORRUPTED_QUATERNION"));
+      }
+    }
   }
-#if 0
-  Serial.println(millis()%100);
-#endif
-  if (!getYPR())
-    return;
-    
-  count_presample();
-  
-  if(presample_count < 0 && !in_error)
-    position_adjust();
+}
+
+void disable_mpu() {
+  mpu_set_dmp_state(0);
+  dmp_on = 0;
+}
+
+void enable_mpu() {
+  mpu_set_dmp_state(1);  // This enables the DMP; at this point, interrupts should commence
+  dmp_on = 1;
+}
+
+void getGravity(struct FloatVectorStruct *v, Quaternion *q) {
+
+  v -> x = 2 * (q -> x * q -> z - q -> w * q -> y);
+  v -> y = 2 * (q -> w * q -> x + q -> y * q -> z);
+  v -> z = q -> w * q -> w - q -> x * q -> x - q -> y * q -> y + q -> z * q -> z;
+}
+
+void dmpGetYawPitchRoll(float *data, Quaternion *q, struct FloatVectorStruct *gravity) {
+
+  // yaw: (about Z axis)
+  data[0] = atan2(2 * q -> x * q -> y - 2 * q -> w * q -> z, 2 * q -> w * q -> w + 2 * q -> x * q -> x - 1);
+  // pitch: (nose up/down, about Y axis)
+  data[1] = atan(gravity -> x / sqrt(gravity -> y * gravity -> y + gravity -> z * gravity -> z));
+  // roll: (tilt left/right, about X axis)
+  data[2] = atan(gravity -> y / sqrt(gravity -> x * gravity -> x + gravity -> z * gravity -> z));
 }
 
 void reset_pid_output(void)
@@ -202,62 +368,148 @@ void initServo(void)
   ESC_D_REG = ESC_REG_LOW;
   ESC_T_REG = ESC_REG_LOW;
 }
+void initMPU() {
+  // MPU-6050 Initialization
 
-void count_presample(void)
-{
-  if (in_error) return;
-  
-  if (presample_count > PRESAMPLE_STABLE_CHECK) {
-    presample_count --;
-    if (presample_count % 100 == 50) digitalWrite(13, LOW);
-    if (presample_count % 100 == 1) digitalWrite(13, HIGH);
-    if (presample_count % 100 == 0) Serial.println(presample_count / 100);
-  } else if (presample_count == PRESAMPLE_STABLE_CHECK) {
-    presample_count --;
-    resample_sensor = true;
-  } else if (presample_count < PRESAMPLE_STABLE_CHECK && presample_count >= 0) {
-    presample_count --;
-    if (accel_z > PRESAMPLE_STABLE_TOLERANCE || accel_z < -PRESAMPLE_STABLE_TOLERANCE) in_error = 1;
-    
-    if (in_error) {
-      digitalWrite(13, HIGH);
-      presample_count = PRESAMPLE_COUNT;
-      in_error = 0;
-      initMPU();
+  // Initialize the MPU:
+  //
+  // Gyro sensitivity:      2000 degrees/sec
+  // Accel sensitivity:     2 g
+  // Gyro Low-pass filter:  42Hz
+  // DMP Update rate:       10Hz
+
+  boolean mpu_initialized = false;
+  while (!mpu_initialized) {
+    if (initialize_mpu()) {
+      mpu_initialized = true;
+      Serial.println(F("Success"));
+      enable_mpu();
+    } else {
+      Serial.println(F("Failed"));
+      mpu_force_reset();
+      delay(100);
+      Serial.println(F("Re-initializing"));
     }
   }
+  Serial.println(F("MPU Initialized"));
 }
 
-void initMPU(){
-  int i;
-  
-  mpu.initialize();
-  in_error = mpu.dmpInitialize();
+void resetI2C(void)
+{
+  // reset I2C bus
+  // This ensures that if the nav6 was reset, but the devices
+  // on the I2C bus were not, that any transfer in progress do
+  // not hang the device/bus.  Since the MPU-6050 has a 1024-byte
+  // fifo, and there are 8 bits/byte, 10,000 clock edges
+  // are generated to ensure the fifo is completely cleared
+  // in the worst case.
 
-  //mpu.setRate(2);
+  pinMode(SDA_PIN, INPUT);
+  pinMode(SCL_PIN, OUTPUT);
 
-#if 0
-  mpu.setXAccelOffset(-197);
-  mpu.setYAccelOffset(-1983);
-  mpu.setZAccelOffset(676);
-  mpu.setXGyroOffset(76);
-  mpu.setYGyroOffset(-32);
-  mpu.setZGyroOffset(69);
-#endif
+  // Clock through up to 1000 bits
+  int x = 0;
+  for ( int i = 0; i < 10000; i++ ) {
 
-  if(in_error == 0){
-    attachInterrupt(digitalPinToInterrupt(MPU_INT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-    packetSize = mpu.dmpGetFIFOPacketSize();
-    mpu.setDMPEnabled(true);
-  } else {
-    Serial.println(F("E."));
-    digitalWrite(13, 1);
+    digitalWrite(SCL_PIN, HIGH);
+    digitalWrite(SCL_PIN, LOW);
+    digitalWrite(SCL_PIN, HIGH);
+
+    x++;
+    if ( x == 8 ) {
+      x = 0;
+      // send a I2C stop signal
+      pinMode(SDA_PIN, OUTPUT);
+      digitalWrite(SDA_PIN, HIGH);
+      digitalWrite(SDA_PIN, LOW);
+      pinMode(SDA_PIN, INPUT);
+    }
   }
+
+  // send a I2C stop signal
+  pinMode(SDA_PIN, OUTPUT);
+  digitalWrite(SDA_PIN, HIGH);
+  digitalWrite(SDA_PIN, LOW);
 }
 
-inline void dmpDataReady() {
-    mpuInterrupt = true;
+boolean initialize_mpu() {
+  int result;
+  struct int_param_s int_param;
+
+  /* Set up gyro.
+   * Every function preceded by mpu_ is a driver function and can be found
+   * in inv_mpu.h.
+   */
+  pinMode(2, INPUT);
+  int_param.cb = gyro_data_ready_cb;
+  int_param.pin = digitalPinToInterrupt(2);
+  result = mpu_init(&int_param);
+  if (result != 0) {
+    Serial.println(F("mpu_init failed!"));
+    return false;
+  }
+
+  /* Get/set hardware configuration. Start gyro. */
+  /* Wake up all sensors. */
+  mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL);
+  /* Push both gyro and accel data into the FIFO. */
+  mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL);
+  mpu_set_sample_rate(DEFAULT_MPU_HZ);
+  /* Read back configuration in case it was set improperly. */
+  mpu_get_sample_rate(&dmp_update_rate);
+  mpu_get_gyro_fsr(&gyro_fsr);
+  mpu_get_accel_fsr(&accel_fsr);
+
+  /* To initialize the DMP:
+   * 1. Call dmp_load_motion_driver_firmware(). This pushes the DMP image in
+   *    inv_mpu_dmp_motion_driver.h into the MPU memory.
+   * 2. Push the gyro and accel orientation matrix to the DMP.
+   * 3. Register gesture callbacks. Don't worry, these callbacks won't be
+   *    executed unless the corresponding feature is enabled.
+   * 4. Call dmp_enable_feature(mask) to enable different features.
+   * 5. Call dmp_set_fifo_rate(freq) to select a DMP output rate.
+   * 6. Call any feature-specific control functions.
+   *
+   * To enable the DMP, just call mpu_set_dmp_state(1). This function can
+   * be called repeatedly to enable and disable the DMP at runtime.
+   *
+   * The following is a short summary of the features supported in the DMP
+   * image provided in inv_mpu_dmp_motion_driver.c:
+   * DMP_FEATURE_LP_QUAT: Generate a gyro-only quaternion on the DMP at
+   * 200Hz. Integrating the gyro data at higher rates reduces numerical
+   * errors (compared to integration on the MCU at a lower sampling rate).
+   * DMP_FEATURE_6X_LP_QUAT: Generate a gyro/accel quaternion on the DMP at
+   * 200Hz. Cannot be used in combination with DMP_FEATURE_LP_QUAT.
+   * DMP_FEATURE_TAP: Detect taps along the X, Y, and Z axes.
+   * DMP_FEATURE_ANDROID_ORIENT: Google's screen rotation algorithm. Triggers
+   * an event at the four orientations where the screen should rotate.
+   * DMP_FEATURE_GYRO_CAL: Calibrates the gyro data after eight seconds of
+   * no motion.
+   * DMP_FEATURE_SEND_RAW_ACCEL: Add raw accelerometer data to the FIFO.
+   * DMP_FEATURE_SEND_RAW_GYRO: Add raw gyro data to the FIFO.
+   * DMP_FEATURE_SEND_CAL_GYRO: Add calibrated gyro data to the FIFO. Cannot
+   * be used in combination with DMP_FEATURE_SEND_RAW_GYRO.
+   */
+  result = dmp_load_motion_driver_firmware();
+  if ( result != 0 ) {
+    Serial.println("firmware load failure");
+    return false;
+  }
+  dmp_set_orientation(0x88);
+
+  unsigned short dmp_features = DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_SEND_RAW_ACCEL |
+                                DMP_FEATURE_SEND_CAL_GYRO | DMP_FEATURE_GYRO_CAL;
+  dmp_enable_feature(dmp_features);
+  dmp_set_fifo_rate(DEFAULT_MPU_HZ);
+  return true;
+}
+
+/* Every time new gyro data is available, this function is called in an
+ * ISR context. In this example, it sets a flag protecting the FIFO read
+ * function.
+ */
+void gyro_data_ready_cb(void) {
+  new_gyro = true;
 }
 
 void set_servos(void)
@@ -305,10 +557,8 @@ void set_servos(void)
   Serial.println(gyro_y); 
   Serial.print(F("bal_axes: "));
   Serial.println(bal_axes);
-
   Serial.print(F("t : "));
   Serial.println(millis());
- 
   Serial.print(F("adj_accel_z : "));
   Serial.println(adj_accel_z);
   Serial.print(F("accel_z : "));
@@ -328,7 +578,6 @@ void set_servos(void)
 
 void reset_adjust_variables(void)
 {
-  adj_accel_z = 0;
   adj_gyro_x = adj_gyro_y  = 0;
 }
 
@@ -412,49 +661,6 @@ void position_adjust(void)
   yPID.Compute();
   
   set_servos();
-}
-
-boolean getYPR(){
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-  fifoCount = mpu.getFIFOCount();
-
-  if((mpuIntStatus & 0x10) || fifoCount >= 1024){
-    mpu.resetFIFO(); 
-    Serial.print(F("rf : "));
-    Serial.println(millis());
-    return false;
-  } else if(mpuIntStatus & 0x02){
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-    fifoCount -= packetSize;
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-    if (resample_sensor) {
-      resample_sensor = false;
-        
-      Serial.println(F("R."));
-    }
-
-    ypr[2] = ypr[2] * 180 / M_PI;
-    ypr[1] = ypr[1] * 180 / M_PI;
-    ypr[0] = ypr[0] * 180 / M_PI;
-
-    if(abs(ypr[0]-yprLast[0])>30) ypr[0] = yprLast[0];
-    if(abs(ypr[1]-yprLast[1])>30) ypr[1] = yprLast[1];
-    if(abs(ypr[2]-yprLast[2])>30) ypr[2] = yprLast[2];
-
-    yprLast[0] = ypr[0];
-    yprLast[1] = ypr[1];
-    yprLast[2] = ypr[2];
-
-    gyro_x = ypr[2];
-    gyro_y = - ypr[1];
-
-    return true;
-  }
 }
 
 void arm(int delay_req)
